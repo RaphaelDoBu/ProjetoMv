351e8a0d8bb717d0f2101a96b16f5b32e579e2a7
Jose Raphael Santos do Bu <jose.bu@ccc.ufcg.edu.br>
2020-06-30 18:37:05 -0300

    modificação no nome da classe e properties

--- Projeto-MV/src/main/java/com/projeto/mv/controller/ProdutoController.java
+++ Projeto-MV/src/main/java/com/projeto/mv/controller/ProdutoController.java
@@ -6,6 +6,7 @@ import java.util.Optional;
 import javax.persistence.PostPersist;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PatchMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -19,39 +20,39 @@ import com.projeto.mv.model.Produto;
 import com.projeto.mv.repository.ProdutoRepository;
 
 @RestController
-@RequestMapping("/produtos")
-public class ProdutosController {
+@RequestMapping("/produto")
+public class ProdutoController {
 	
 	@Autowired
 	private ProdutoRepository produtoRepository;
 	
-	@GetMapping(value = "/")
+	@GetMapping(value = "/lista")
 	public List<Produto> findAll(){
 		return produtoRepository.findAll();		
 	}
 	
-	@GetMapping(value = "/{codigoProduto}")
+	@GetMapping(value = "/id/{codigoProduto}")
 	public Optional<Produto> findByCodigoProduto(@PathVariable final long codigoProduto){
 		return produtoRepository.findById(codigoProduto);		
 	}
 	
-	@PostMapping(value="/")
+	@PostMapping(value="/novo")
 	public Optional<Produto> saveProduto(@RequestBody final Produto produto){
 		produtoRepository.save(produto);
 		return produtoRepository.findById(produto.getCodigoProduto());
 	}
 	
-	@PutMapping(value = "/{codigoProduto}")
-	public Optional<Produto> putProduto(@PathVariable final long codigoProduto){
-		return produtoRepository.findById(codigoProduto);		
-	}
+//	@PutMapping(value = "/{codigoProduto}")
+//	public Optional<Produto> putProduto(@PathVariable final long codigoProduto){
+//		return produtoRepository.findById(codigoProduto);		
+//	}
+//	
+//	@PatchMapping(value = "/{codigoProduto}")
+//	public Optional<Produto> patchProduto(@PathVariable final long codigoProduto){
+//		return produtoRepository.findById(codigoProduto);		
+//	}
 	
-	@PatchMapping(value = "/{codigoProduto}")
-	public Optional<Produto> patchProduto(@PathVariable final long codigoProduto){
-		return produtoRepository.findById(codigoProduto);		
-	}
-	
-	@PatchMapping(value = "/{codigoProduto}")
+	@DeleteMapping(value = "/{codigoProduto}")
 	public void deleteProduto(@PathVariable final long codigoProduto){
 		produtoRepository.deleteById(codigoProduto);		
 	}
